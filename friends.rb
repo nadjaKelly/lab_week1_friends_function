c9ca6ef99a29d76f338ca77a001c2caa86e16312
Nadja Kelly <nmfort-info@yahoo.de>
2018-11-24 20:44:20 +0000

    friends.rb

--- friends.rb
+++ friends.rb
@@ -1,3 +1,4 @@
+# 1
 def get_name(person)
   return person[:name]
 end
@@ -5,4 +6,59 @@ end
 
 #######################
 
-# def favorite_tv_show(person)
+# 2
+def favorite_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+
+
+#######################
+
+# 3
+def like_food(person, food)
+  for one_element in person[:favourites][:snacks]
+    if one_element == food
+  return true
+    end
+  end
+return false
+end
+
+#######################
+
+# 4
+def add_name(person, friend_name)
+  person[:friends].push(friend_name)
+end
+
+#######################
+
+# 5
+def remove_name(person, friend_name)
+  person[:friends].delete(friend_name)
+end
+
+#######################
+
+# # 6
+#
+  def total_money(people)
+
+    total = 0
+  for one_element in people
+    total += one_element[:monies]
+  end
+    return total
+
+  end
+
+  #############################
+
+#   def total_money(people)
+#   total_money = 0
+#   for person in people
+#     total_money += person[:monies]
+#   end
+#   return total_money
+# end
