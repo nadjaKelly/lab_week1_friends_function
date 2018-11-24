46fbe93df8a98c945026ef9ee15cf791d2fd8907
Nadja Kelly <nmfort-info@yahoo.de>
2018-11-24 17:58:33 +0000

    test git

--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -73,17 +73,17 @@ class TestFriends < MiniTest::Test
 
   # 2. For a given person, return their favourite tv show
   # (e.g. the function favourite_tv_show(@person2) should return the string "Baywatch")
-  # def test_favorite_tv_show
-  #   result = favorite_tv_show(@person1)
-  #   assert_equal("Friends", result)
-  # end
+  def test_favorite_tv_show
+    result = favorite_tv_show(@person1)
+    assert_equal("Friends", result)
+  end
 
 
   # 3. For a given person, check if they like a particular food
   # (e.g. the function likes_to_eat(@person2, "bread") should return true, likes_to_eat(@person3, "spinach") should return false)
   def test_like_food
     result = like
-
+end 
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(@person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
   # (hint: This function should not return anything. After the function call, check for the length of the friends array to test it!)
@@ -108,5 +108,4 @@ class TestFriends < MiniTest::Test
 
   # 9. Find people with no friends
   # (hint: return an array, there might be more people in the future with no friends!)
-
 end
